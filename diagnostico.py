f7547638d64b9d9504f8c4c675ad12bd99f4ff75
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-05-14 23:13:42 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -2,4 +2,9 @@ import pandas as pd
 import streamlit as st
 
 dados = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
-dados
+
+crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique())
+municipio = st.selectbox('Selecione o município', options=dados[dados['Regional de Saúde']==crs]['Município'].unique())
+tipo_forma_abastecimento = st.selectbox('Selecione o município', options=dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique())
+dados_municipio = dados[(dados['Município']==municipio)&(dados['Tipo da Forma de Abastecimento']==tipo_forma_abastecimento)]
+st.dataframe(dados_municipio)
