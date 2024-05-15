e5204df15a2a318f493fdc125ba3ef667e372b6f
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-05-14 23:15:34 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -3,8 +3,12 @@ import streamlit as st
 
 dados = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
 
-crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique())
-municipio = st.selectbox('Selecione o município', options=dados[dados['Regional de Saúde']==crs]['Município'].unique())
-tipo_forma_abastecimento = st.selectbox('Selecione o município', options=dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique())
+crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique(), index=None)
+municipio = st.selectbox('Selecione o município', options=dados[dados['Regional de Saúde']==crs]['Município'].unique(), index=None)
+tipo_forma_abastecimento = st.selectbox('Selecione o município', options=dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique(), index=None)
 dados_municipio = dados[(dados['Município']==municipio)&(dados['Tipo da Forma de Abastecimento']==tipo_forma_abastecimento)]
-st.dataframe(dados_municipio)
+try:
+  st.dataframe(dados_municipio)
+
+except:
+  st.write('')
