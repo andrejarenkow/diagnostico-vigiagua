69482e480ea8482dd3c8b6ba893c1bf924b52fe2
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-15 11:35:48 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -25,34 +25,36 @@ def load_data(url):
     return df
 
 dados = load_data('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
-
+col1,colcenter2,col3 = st.columns(3)
 # Cria um seletor para escolher a Regional de Saúde
-crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique(), index=None, placeholder='Selecione uma CRS', key='crs')
-
-# Cria um seletor para escolher o município com base na Regional de Saúde selecionada
-municipio = st.selectbox('Selecione o município', options=sorted(dados[dados['Regional de Saúde']==crs]['Município'].unique()), index=None, placeholder='Selecione uma município', key='municipio')
-
-# Cria um seletor para escolher o tipo da forma de abastecimento com base no município selecionado
-tipo_forma_abastecimento = st.selectbox('Selecione o tipo da forma de abastecimento', options=sorted(dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique()), index=None, placeholder='Selecione um tipo de forma de abastecimento', key='forma')
+with colcenter2:
+    crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique(), index=None, placeholder='Selecione uma CRS', key='crs')
+    
+    # Cria um seletor para escolher o município com base na Regional de Saúde selecionada
+    municipio = st.selectbox('Selecione o município', options=sorted(dados[dados['Regional de Saúde']==crs]['Município'].unique()), index=None, placeholder='Selecione uma município', key='municipio')
+    
+    # Cria um seletor para escolher o tipo da forma de abastecimento com base no município selecionado
+    tipo_forma_abastecimento = st.selectbox('Selecione o tipo da forma de abastecimento', options=sorted(dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique()), index=None, placeholder='Selecione um tipo de forma de abastecimento', key='forma')
 
 # Filtra os dados para exibir apenas as informações relevantes com base no município e tipo da forma de abastecimento selecionados
 dados_municipio = dados[(dados['Município']==municipio)&(dados['Tipo da Forma de Abastecimento']==tipo_forma_abastecimento)][['Nome da Forma de Abastecimento','Sem informação', 'Funcionando', 'Parada/danificada']]
-
+col4,colcenter5,col6 = st.columns([1,2,1)
 try:
     # Tenta executar as seguintes instruções
     # Comentários abaixo são comentários de código, não estão habilitados no momento devido ao formato da entrada.
     # st.subheader(f'{tipo_forma_abastecimento} no município de {municipio}')
-    st.write('Marque o status de cada uma para informar seu status')  # Exibe uma mensagem para o usuário
-    st.data_editor(dados_municipio, use_container_width=True, hide_index=True)  # Exibe os dados do município para edição
-
-    # Cria um botão para enviar a atualização e redefine o estado da sessão quando clicado
-    submit = st.button('Enviar atualização!', type='primary', on_click=reset)
-
-    # Verifica se o botão de envio foi clicado
-    if submit:
-        # Exibe uma mensagem de sucesso quando a atualização é enviada
-        st.success('Atualização enviada!', icon="✅")
-        st.cache_data.clear()  # Limpa o cache de dados
+    with colcenter5:
+        st.write('Marque o status de cada uma para informar seu status')  # Exibe uma mensagem para o usuário
+        st.data_editor(dados_municipio, use_container_width=True, hide_index=True)  # Exibe os dados do município para edição
+    
+        # Cria um botão para enviar a atualização e redefine o estado da sessão quando clicado
+        submit = st.button('Enviar atualização!', type='primary', on_click=reset)
+    
+        # Verifica se o botão de envio foi clicado
+        if submit:
+            # Exibe uma mensagem de sucesso quando a atualização é enviada
+            st.success('Atualização enviada!', icon="✅")
+            st.cache_data.clear()  # Limpa o cache de dados
 
 except:
     # Se ocorrer uma exceção, exibe uma mensagem em branco
