4ee186a4dedf02c1449a7c44fd1d7f654d874623
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-05-14 23:06:19 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-dados = pd.read_xlsx('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
+dados = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
 dados
