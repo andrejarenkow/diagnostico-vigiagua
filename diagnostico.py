f17707292c9b5536b016f4443a30c908565e6237
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-05-14 23:19:46 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -4,7 +4,7 @@ import streamlit as st
 dados = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQORvrZiO6l26dKcj9alqq76X1sP7IdLjfSwu-FVhj2b3pM8PvjPGVEHcDt6nhhIkFXy-utm9FIQ9/pub?output=xlsx')
 
 crs = st.selectbox('Selecione a CRS', options=dados['Regional de Saúde'].unique(), index=None, placeholder='Selecione uma CRS')
-municipio = st.selectbox('Selecione o município', options=dados[dados['Regional de Saúde']==crs]['Município'].unique(), index=None, placeholder='Selecione uma município')
+municipio = st.selectbox('Selecione o município', options=sorted(dados[dados['Regional de Saúde']==crs]['Município'].unique()), index=None, placeholder='Selecione uma município')
 tipo_forma_abastecimento = st.selectbox('Selecione o tipo da forma de abastecimento', options=sorted(dados[dados['Município']==municipio]['Tipo da Forma de Abastecimento'].unique()), index=None, placeholder='Selecione um tipo de forma de abastecimento')
 dados_municipio = dados[(dados['Município']==municipio)&(dados['Tipo da Forma de Abastecimento']==tipo_forma_abastecimento)]
 try:
