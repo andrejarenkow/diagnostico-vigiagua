d0402e2650ca17be792adc0c08e3e053d4c6fe4f
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-15 22:16:56 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -146,7 +146,8 @@ with container_data_editor:
             <style>
             #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(5) > div > div > div > div.st-emotion-cache-keje6w.e1f1d6gn3 > div > div > div > div:nth-child(3) > div > button {{
                 display: flex;
-                justify-content: center; /* Centraliza horizontalmente */
+                align-items: center; /* Centraliza horizontalmente */
+                
             }}
             </style>
             ''')
