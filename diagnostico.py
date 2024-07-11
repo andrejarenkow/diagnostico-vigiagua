1ab18d9ea01b3bb6646ea748344fbaa8f0fb426c
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-11 15:48:31 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -71,7 +71,7 @@ st_echarts(
 ##########################################################
 
 c = (
-    Pie()
+    st_pyecharts.charts.Pie()
     .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
     .set_global_opts(title_opts=opts.TitleOpts(title="Pie-基本示例"))
     .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
