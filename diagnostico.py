0471d9b598fee376afe48de02c4739b26f514660
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-11 13:37:38 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -2,3 +2,15 @@ import pandas as pd  # Importa a biblioteca pandas e a renomeia como pd
 import streamlit as st # Importa a biblioteca streamlit e a renomeia como st
 
 st.title('area de testes')
+
+option = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
+}
+st_echarts(
+    options=option, height="400px",
+)
