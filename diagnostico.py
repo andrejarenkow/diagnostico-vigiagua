23447b2ce3d3ec9f2fa4f26acdea5260a79768f6
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-11 13:43:10 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -17,7 +17,7 @@ option = {
     },
     "xAxis": {
         "type": "category",
-        "data": ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+        "data": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
         "axisPointer": {
             "type": "shadow"
         }
