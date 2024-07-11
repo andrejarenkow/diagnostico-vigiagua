b39065753109c7d101abb250a6ce6d9609cefb50
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-11 13:38:29 -0300

    Update diagnostico.py

--- diagnostico.py
+++ diagnostico.py
@@ -1,5 +1,7 @@
 import pandas as pd  # Importa a biblioteca pandas e a renomeia como pd
 import streamlit as st # Importa a biblioteca streamlit e a renomeia como st
+from streamlit_echarts import st_echarts
+
 
 st.title('area de testes')
 
